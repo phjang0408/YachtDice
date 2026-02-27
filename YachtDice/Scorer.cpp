d23875929c3316c996c48ddb651cd3effade7282
phjang0408 <phjang0408@naver.com>
2026-02-27 19:12:25 +0900

    Scorer 구현

--- YachtDice/Scorer.cpp
+++ YachtDice/Scorer.cpp
@@ -1,7 +1,7 @@
 #include "Scorer.h"
 
 std::array<ScoreSlot, static_cast<size_t>(ScoreCategory::COUNT)>
-MakePreviewScores(std::vector<int>& dice) {
+Scorer::MakePreviewScores(std::vector<int>& dice) {
 	// 1. 빈도수 배열 만들기
 	std::array<int, 6> freq = makeFreqArray(dice);
 
@@ -18,10 +18,10 @@ MakePreviewScores(std::vector<int>& dice) {
 	return Prev_ScoreBoard;
 }
 
-std::array<int, 6> makeFreqArray(const std::vector<int>& diceVec) {
+std::array<int, 6> Scorer::makeFreqArray(const std::vector<int>& diceVec) {
 	std::array<int, 6> freq = { 0 };
 
-	// diceVec의값 - 1은 freq의 index (1~6 에서 0~5로)
+	// (diceVec의 값 - 1) = freq의 index (1~6 에서 0~5로)
 	for (auto it : diceVec) {
 		freq[it - 1]++;
 	}
@@ -29,7 +29,7 @@ std::array<int, 6> makeFreqArray(const std::vector<int>& diceVec) {
 }
 
 // category에 따라서, 계산 방법을 다르게 
-int CalcByCategory(ScoreCategory category, 	const std::vector<int>&dice, 
+int Scorer::CalcByCategory(ScoreCategory category, 	const std::vector<int>&dice,
 	const std::array<int, 6>& freq) {
 	switch (category) {
 	case(ScoreCategory::Ones):
@@ -38,12 +38,89 @@ int CalcByCategory(ScoreCategory category, 	const std::vector<int>&dice,
 	case(ScoreCategory::Fours):
 	case(ScoreCategory::Fives):
 	case(ScoreCategory::Sixes):
-		CalcUpper(category, freq);
+		return Scorer::Calc_Upper(category, freq);
+	case(ScoreCategory::Choice):
+		return Scorer::Calc_Choice(dice);
+	case(ScoreCategory::FourOfKind):
+		return Scorer::Calc_FourOfKind(freq);
+	case(ScoreCategory::FullHouse):
+		return Scorer::Calc_FullHouse(freq);
+	case(ScoreCategory::SmallStraight):
+		return Scorer::Calc_SmallStraight(freq);
+	case(ScoreCategory::LargeStraight):
+		return Scorer::Calc_LargeStraight(freq);
+	case(ScoreCategory::Yacht):
+		return Scorer::Calc_Yacht(freq);
 	}
-
 }
 
-int CalcUpper(ScoreCategory category, const std::array<int, 6> freq) {
+int Scorer::Calc_Upper(ScoreCategory category, const std::array<int, 6> freq) {
 	int CalcUp_Score = static_cast<int>(category);	// 0번부터 시작하니까
 	return freq[CalcUp_Score] * (CalcUp_Score + 1);
+}
+
+int Scorer::Calc_Choice(const std::vector<int>& dice) {
+	int Choice_sum = 0;
+	for (auto it = 0; it < dice.size(); it++) {
+		Choice_sum += dice[it];
+	}
+	return Choice_sum;
+}
+int Scorer::Calc_FourOfKind( const std::array<int, 6>& freq) {
+	int FourOfKind_Score = 0;
+	for (int i = 0; i < freq.size(); i++) {
+		if (freq[i] == 4) {
+			FourOfKind_Score = 4 * (i + 1);
+		}
+	}
+	return FourOfKind_Score;
+}
+
+int Scorer::Calc_FullHouse(const std::array<int, 6>& freq) {
+	bool has_three = false, has_two = false;
+	int FullHouse_Score = 0, three_idx = 0, two_idx = 0;
+	for (int i = 0; i < freq.size(); i++) {
+		if (freq[i] == 3) {
+			has_three = true; three_idx = i;
+		}
+		if (freq[i] == 2) {
+			has_two = true; two_idx = i;
+		}
+	}
+	return has_three && has_two ? (three_idx + 1) * 3 + (two_idx + 1) * 2 : 0;
+}
+int Scorer::Calc_SmallStraight(const std::array<int, 6>& freq) {
+	int is_straight = 0;
+	for (int i = 0; i < freq.size(); i++) {
+		if (freq[i] > 0) {
+			is_straight++;
+			if (is_straight == 4)	return 15;
+		}
+		else {
+			is_straight = 0;
+		}
+	}
+	return 0;
+}
+int Scorer::Calc_LargeStraight(const std::array<int, 6>& freq) {
+	int is_straight = 0;
+	for (int i = 0; i < freq.size(); i++) {
+		if (freq[i] > 0) {
+			is_straight++;
+			if (is_straight == 5)	return 30;
+		}
+		else {
+			is_straight = 0;
+		}
+	}
+	return 0;
+}
+int Scorer::Calc_Yacht(const std::array<int, 6>& freq) {
+	bool has_Yacht = false;
+	for (int i = 0; i < freq.size(); i++) {
+		if (freq[i] == 5) {
+			has_Yacht = true;
+		}
+	}
+	return has_Yacht ? 50 : 0;
 }
--- YachtDice/Scorer.h
+++ YachtDice/Scorer.h
@@ -12,43 +12,11 @@ public:
 private:
 	std::array<int, 6> makeFreqArray(const std::vector<int>& diceVec);
 	int CalcByCategory(ScoreCategory category, const std::vector<int>& dice, const std::array<int, 6>& freq);
-	int CalcUpper(ScoreCategory category, const std::array<int,6> freq);
-	int CalcChoice(std::vector<int>& dice);
+	int Calc_Upper(ScoreCategory category, const std::array<int,6> freq);
+	int Calc_Choice(const std::vector<int>& dice);
+	int Calc_FourOfKind(const std::array<int, 6>& freq);
+	int Calc_FullHouse(const std::array<int, 6>& freq);
+	int Calc_SmallStraight(const std::array<int, 6>& freq);
+	int Calc_LargeStraight(const std::array<int, 6>& freq);
+	int Calc_Yacht(const std::array<int, 6>& freq);
 };
-
-// freq array 반환 함수
-
-// make tempscoreboard
-/*
-* 
-int Calc(ScoreCategory category, const std::vector<int>& diceValues) {
-	// 빈도수 배열
-	std::array<int, 6> count_array = makeFreqArray(diceValues);
-
-	// 빈도수 계산
-	int Aces = count_array[0] * 1;
-	int Deuces = count_array[1] * 2;
-	int Threes = count_array[2] * 3;
-	int Fours = count_array[3] * 4;
-	int Fives = count_array[4] * 5;
-	int Sixes = count_array[5] * 6;
-
-	int Yacht = 0;
-	int Four_of_a_kind = 0;
-	for (int i : count_array) {
-		if (count_array[i] == 4) {
-			Four_of_a_kind = count_array[i] * (i + 1);
-		}
-		else if (count_array[i] == 5) {
-			Yacht = count_array[i] * (i + 1);
-		}
-	}
-	// choice
-	int Choice = 0;
-	for (int d : diceValues) {
-		Choice += d;
-	}
-
-	
-}
-*/
